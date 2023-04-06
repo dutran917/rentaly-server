3e47272bf11232e09ebe349d6ac360c526ac2004
dutran917 <ductcp@gmail.com>
2023-04-06 16:45:19 +0700

    auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
 import { PrismaService } from '../share/prisma.service';
 import { UserRegisterInput } from '../user/dto/user.dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
@@ -18,8 +18,32 @@ export class AuthService {
         email: username,
       },
     });
+    if (!user) {
+      throw new UnauthorizedException('WRONG_CREDENTIALS');
+    }
+    const passwordMatch = await bcrypt.compare(pass, user.password);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('WRONG_CREDENTIALS');
+    }
+    delete user['password'];
+    return {
+      ...user,
+      accessToken: this.generateToken(user.id),
+    };
   }
   async register(input: UserRegisterInput) {
     return this.userService.createUser(input);
+  }
+
+  generateToken(userId: number) {
+    const token = this.jwtService.sign(
+      {
+        id: userId,
+      },
+      {
+        expiresIn: '2d',
+      },
+    );
+    return token;
   }
 }
